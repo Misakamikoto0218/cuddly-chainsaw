2ac0a206058098aee44563af80f5ef1e55ae9ed2
Misakamikoto0218 <xuun005@qq.com>
2025-02-23 22:39:44 +0800

    static

---
+++ test_static/test_static/1.cpp
@@ -0,0 +1,20 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void test()
+{
+	static int a = 1;
+	a++;
+	printf("%d", a);
+
+}
+int main()
+{
+	int x = 0;
+	while (x < 10)
+	{
+		test();
+		x++;
+	}
+	return 0;
+}
